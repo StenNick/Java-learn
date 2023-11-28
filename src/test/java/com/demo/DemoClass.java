19fe384d4938437e5254c2bf63d15919dfbe4c74
n.stenkin <n.stenkin@vk.team>
2023-11-28 18:18:16 +0300

    внес изменения в мастер

--- src/test/java/com/demo/DemoClass.java
+++ src/test/java/com/demo/DemoClass.java
@@ -11,7 +11,7 @@ public class DemoClass {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     LocalDate parseFormat = LocalDate.parse(date, formatter);
     Assertions.assertEquals(DayOfWeek.SATURDAY, parseFormat.getDayOfWeek());
-    // изменения в мастере!
+    // изменения в мастере! Добавили еще что то новое в мастер
   }
 }
 
